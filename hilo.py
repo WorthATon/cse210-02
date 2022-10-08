83739709794f9e14b933177d6e73626491ba2655
syred011 <con22008@byui.edu>
2022-10-08 11:35:20 -0400

    Derizabeth edit

--- hilo.py
+++ hilo.py
@@ -1,42 +1,84 @@
 import random
 
+#This class:
+# -Receives the number of cards (in this case 13)
+# -Reveals the the card randomly
+class Deck:
+    def __init__(self, numberCards):
+        self.numberCards = numberCards
+        self.currentCard = 0
 
-# TODO: Implement the Die class as follows...
+        self.currentCard = self.revealCard()
 
-# 1) Add the class declaration. Use the following class comment.
-class Card:
-    """A small cube with a different number of spots on each of its six sides.
+    def revealCard(self):
+        revealedCard = random.randint(1, self.numberCards)
+        while (revealedCard == self.currentCard):
+            revealedCard = random.randint(1, self.numberCards)
 
-    The responsibility of Die is to keep track of the side facing up and calculate the points for 
-    it.
-   
-    Attributes:
-        value (int): The number of spots on the side facing up.
-        points (int): The number of points the die is worth.
-    """
+        self.currentCard = revealedCard
+        return self.currentCard
 
-    def __init__(self):
-        # 2) Create the class constructor. Use the following method comment.
-        """Constructs a new instance of Die with a value and points attribute.
+#This class:
+# -Receives Player's points
+# -Calculate Player's points  
+class Player:
+    def __init__(self, initialPoints):
+        self.points = initialPoints
 
-        Args:
-            self (Die): An instance of Die.
-        """
-        self.number = 0
-        # self.points = 0
+    def setPoints(self, points):
+        self.points =  self.points + points
 
+#This class:
+# -Initiate other classes (Player and Decks)
+# -Initiate the game
+# -Calculates the guess
+class Game:
+    def __init__(self, initialPoints, rightGuessPoints, wrongGuessPoints, numberCards):
+        self.initialPoints = initialPoints
+        self.rightGuessPoints = rightGuessPoints
+        self.wrongGuessPoints = wrongGuessPoints
+        self.numberCards = numberCards
+        self.player = Player(initialPoints)
+        self.deck = Deck(numberCards)
 
-# 3) Create the roll(self) method. Use the following method comment.
+    def calculateGuess(self, guess):
+        currentCard = self.deck.currentCard
+        revealedCard = self.deck.revealCard()
+        if guess.lower() == "l":
+            if revealedCard < currentCard:
+                game.player.setPoints(self.rightGuessPoints)
+            else:
+                game.player.setPoints(self.wrongGuessPoints)
+        else:
+            if revealedCard > currentCard:
+                game.player.setPoints(self.rightGuessPoints)
+            else:
+                game.player.setPoints(self.wrongGuessPoints)
 
-    def draw(self, new_number):
-        """Generates a new random value and calculates the points.
-        
-        Args:
-            self (Die): An instance of Die.
-        """
-        # self.number = random.randint(1, 13)
-        self.number = new_number
+if __name__ == "__main__":
+    #initiate Game class with the parameters for:
+    # Initial points (300)
+    # Right guess points (100) 
+    # Wrong guess points (-75) 
+    # Number of cards (13)
+    game = Game(300, 100, -75, 13)
 
-    def read(self):
-        return self.number
-        # self.points = 100 if self.value => self.value else 100 if self.value == 1 else 0
+    answer = "y"
+    while answer.lower()== "y":
+        print("")
+        print(f"The card is: {game.deck.currentCard}")
+        guess = input("Higher or lower? [h/l] ")
+        game.calculateGuess(guess)
+
+        if (game.player.points <= 0):
+            print(f"Your score is: {game.player.points}")
+            print("The game is over.")
+            break
+
+        print(f"Next card was: {game.deck.currentCard}")
+        print(f"Your score is: {game.player.points}")
+
+        answer = input("Play again? [y/n] ")
+    else:
+        print("The game is over.")
+
